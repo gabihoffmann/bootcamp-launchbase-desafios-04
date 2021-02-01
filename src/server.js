ca211932ade5fcf9f16e4ae2a7c91661c50a7c4e
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-01 08:54:45 -0300

    :construction:  config server, add nunjucks and routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const path = require("path");
 const express = require("express");
+const nunjucks = require("nunjucks");
+
+const routes = require("./routes");
 
 const app = express();
 
-app.listen(5555, () => {
-  console.log("Server is running in port 5555");
+app.use(express.static(path.resolve(__dirname, "../public")));
+app.use(routes);
+
+app.set("view engine", "njk");
+
+nunjucks.configure(path.resolve(__dirname, "views"), {
+  express: app,
+  autoescape: false,
+  noCache: true,
 });
+
+app.listen(5555, () => console.log("Server is running in port 5555"));
